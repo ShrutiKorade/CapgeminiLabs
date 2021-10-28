fe30690eca98e45ea9e15560d5e75b828c9489fc
ShrutiKorade <69238324+ShrutiKorade@users.noreply.github.com>
2021-10-28 18:19:51 +0530

    new

---
+++ src/lab3/lab3_5.java
@@ -0,0 +1,35 @@
+package lab3;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class lab3_5 {
+	
+
+		static boolean isOrder(String s) {
+			int n =s.length();
+			char[] ch =new char[n];
+			for(int i =0;i<n;i++)
+			{
+				ch[i] =s.charAt(i);
+			}
+			Arrays.sort(ch);
+			for(int i=0;i<n;i++)
+			{
+				if(ch[i]!=s.charAt(i))
+					return false;
+			}
+			return true;
+		}
+		public static void main(String[] args) {
+			  
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Enter your String ");
+			String s =sc.nextLine();
+			if(isOrder(s))
+			System.out.println("postive String");
+			else
+		    System.out.println("Negative String");
+			sc.close();
+		}
+	}
+
---
+++ src/lab3/lab3_7.java
@@ -0,0 +1,24 @@
+package lab3;
+import java.time.*;
+public class lab3_7 {
+
+	public static void main(String[] args)
+	{
+		LocalDate pdate = LocalDate.of(1999, 8 ,01);
+		LocalDate now = LocalDate.now();
+
+		Period diff = Period.between(pdate, now);
+
+		System.out.printf("\nDifference is %d years, %d months and %d days old:", 
+				diff.getYears(), diff.getMonths(), diff.getDays());
+	}
+}
+
+
+
+
+
+
+
+
+
